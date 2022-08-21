f26415ebe1b88644d9b36cf090a28a9d9c18240d
Daniel Chen <chendaniely@gmail.com>
2022-08-21 14:14:22 -0700

    feature: plumber API for just the ggplot

---
+++ pictaACTinfographics/shiny/plumber.R
@@ -0,0 +1,93 @@
+source("global.R")
+
+#* Return the Asthma Image
+#* @param language Language, one of: "english", or "spanish"
+#* @param display_name Name of patient, defaults to ""
+#* @param today_act Current ACT score
+#* @param today_date Current date for ACT score, YYYY-MM-DD
+#* @param previous_act Previous ACT score
+#* @param previous_date Previous date for ACT score, YYYY-MM-DD
+#* @param dummy_data Empty R dataframe to draw the ggplot2 canvas
+#* @serializer contentType list(type='image/png')
+#* @post /gen_image
+gen_image <- function(
+  language = "english",
+  display_name = "",
+  today_act = 25,
+  today_date = "2022-08-21",
+  previous_act = 5,
+  previous_date = "2021-07-14",
+  dummy_data = dummy
+) {
+
+  today_act <- as.integer(today_act)
+  previous_act <- as.integer(previous_act)
+
+  # patient info -----
+  print(glue::glue(
+    "Plumber: Generating patient info."
+  ))
+  pt_info <- gen_pt_info(
+    display_name = display_name,
+    language = language,
+    today_date = today_date,
+    today_act_score = today_act,
+    previous_date = previous_date,
+    previous_act_score = previous_act
+  )
+
+  # plot info -----
+  print(glue::glue(
+    "Plumber: Generating plot info."
+  ))
+  plot_info <- list(
+    arrow_x_all = gen_x_coords(pt_info$language),
+    image = png::readPNG(pt_info$png_url)
+  )
+  plot_info$base_image_g <- grid::rasterGrob(
+    plot_info$image, interpolate = TRUE
+  )
+
+  plot_info$plot_pth_norm <- fs::path_norm(tempfile(fileext = '.png'))
+  plot_info$plot_pth_unix <- gsub("\\\\", "/", plot_info$plot_pth_norm)
+  plot_info$fig_pth_act_exterior <- gen_exterior_png_pth(
+    pt_info$language
+  )
+
+  print(glue::glue(
+    "Plumber: Generating base plot."
+  ))
+  base_g <- geom_base_image(plot_info$base_image_g)
+
+  print(glue::glue(
+    "Plumber: Generating plot from base."
+  ))
+  arrow_g <- geom_score_arrows(
+    base_g = base_g,
+    today_act = pt_info$today_act,
+    previous_act = pt_info$previous_act,
+    previous_date = pt_info$previous_date_text,
+    language = pt_info$language,
+    x_breaks = plot_info$arrow_x_all
+  )
+
+  print(glue::glue(
+    "Plumber: Returning plot."
+  ))
+  # https://www.r-bloggers.com/2021/01/how-to-make-rest-apis-with-r-a-beginners-guide-to-plumber/
+  file <- "plot.png"
+  ggsave(
+    filename = file,
+    arrow_g,
+    scale = 1,
+    width = 11,
+    height = 8.5,
+    units = "in",
+    dpi = 300)
+  readBin(file, "raw", n = file.info(file)$size)
+}
+
+# #debugonce(geom_base_image)
+# gen_image()
+
+# gen_image(today_act = 6)
